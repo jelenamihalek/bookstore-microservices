8e120d473938f5df6e8858de50c7b8075c132f56
jelenamihalek <jelena.mihalek123@gmail.com>
2026-03-14 14:16:34 +0100

    Initial microservices setup

---
+++ api-gateaway/src/main/java/com/bookstore/api_gateaway/ApiGateawayApplication.java
@@ -0,0 +1,13 @@
+package com.bookstore.api_gateaway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGateawayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGateawayApplication.class, args);
+	}
+
+}
---
+++ api-gateaway/src/test/java/com/bookstore/api_gateaway/ApiGateawayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookstore.api_gateaway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGateawayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
